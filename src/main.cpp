90b5404fc8ce1d831a64d47487ad829d8312ea10
Mohammad Ali Zahir <m.alizahir786@gmail.com>
2023-10-18 03:28:14 -0400

    Added check for invalid input

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,17 @@ int main() {
 
     // Input the Sudoku grid manually
     std::cout << "Enter the Sudoku grid (use 0 for empty cells):\n";
-    for (int row = 0; row < N; row++) {
-        for (int col = 0; col < N; col++) {
-            std::cin >> grid[row][col];
+    for (int row = 0; row < 9; row++) {
+        for (int col = 0; col < 9; col++) {
+            std::string input;
+            std::cin >> input;
+
+            if (input.length() != 1 || !isdigit(input[0])) {
+                std::cerr << "Invalid input. Please enter a single digit or 0 for empty cells." << std::endl;
+                return 1;
+            }
+
+            grid[row][col] = input[0] - '0';
         }
     }
 
